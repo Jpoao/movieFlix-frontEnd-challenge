f129b553d05ae8fcc79a3aaafa9603ed7c1d2823
João Paulo <mg_ses@hotmail.com>
2021-08-30 12:43:09 -0300

    adding bootstrap, basic app styles, routes, home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Routes from 'Routes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>HOME PAGE</h1>
+        <Routes />
     </div>
   );
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.css"
+
+const Home = () => {
+    return(
+        <h1>HOME</h1>
+    );
+};
+
+export default Home;
