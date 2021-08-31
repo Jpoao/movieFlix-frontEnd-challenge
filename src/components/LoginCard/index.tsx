dc21db370eb490618993195a6e09f5bbb3e65726
João Paulo <mg_ses@hotmail.com>
2021-08-31 12:51:01 -0300

    api auth and storage credencials implemetation + login error message

--- src/components/LoginCard/index.tsx
+++ src/components/LoginCard/index.tsx
@@ -1,25 +1,65 @@
 import ButtonIcon from "components/ButtonIcon";
+import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useHistory, useLocation } from "react-router-dom";
+import { AuthContext } from "AuthContext";
+import { saveAuthData } from "util/storage";
 import "./styles.css";
+import { getTokenData } from "util/auth";
+import { requestBackendLogin } from "util/requests";
 
 type FormData = {
   username: string;
   password: string;
 };
 
-const LoginCard = () => {
-  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
-  
-  const onSubmit =(formData: FormData) => {
-    console.log(formData.username);
-    console.log(formData.password);
-  };
+type LocationState = {
+  from: string;
+};
 
+const LoginCard = () => {
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: "/movies" } };
+
+  const { setAuthContextData } = useContext(AuthContext);
+
+  const [hasError, setHasError] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const history = useHistory();
+
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        setHasError(false);
+        saveAuthData(response.data);
+        setAuthContextData({
+          authenticated: true,
+          tokenData: getTokenData(),
+        });
+        history.replace(from);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log("erro", error);
+      });
+  };
 
   return (
     <div className="login-card-container">
       <h1>LOGIN</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
+        {hasError && (
+          <div className="alert alert-danger">
+            Erro ao tentar efetuar o login
+          </div>
+        )}
         <div className="mb-4">
           <input
             {...register("username", {
@@ -30,27 +70,31 @@ const LoginCard = () => {
               },
             })}
             type="text"
-            className={`form-control base-input ${errors.username ? "is-invalid" : ""}`}
+            className={`form-control base-input ${
+              errors.username ? "is-invalid" : ""
+            }`}
             placeholder="Email*"
             name="username"
           />
-           <div className="invalid-feedback d-block">{errors.username?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
             {...register("password", {
               required: "Campo obrigatório!",
-              pattern: {
-                value: /^.{6,}$/i,
-                message: "Senha invalida",
-              },
             })}
             type="password"
-            className={`form-control base-input ${errors.password ? "is-invalid" : ""}`}
+            className={`form-control base-input ${
+              errors.password ? "is-invalid" : ""
+            }`}
             placeholder="Password*"
             name="password"
           />
-           <div className="invalid-feedback d-block">{errors.password?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <div className="login-submit">
           <ButtonIcon text="FAZER LOGIN" />
