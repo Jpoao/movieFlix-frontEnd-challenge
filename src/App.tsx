f882022685802eb76f003458b1caf834ede0871e
João Paulo <mg_ses@hotmail.com>
2021-08-31 14:26:24 -0300

    logout implemetation, global auth context, auto redirecting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import Routes from 'Routes';
 import './assets/styles/custom.scss'
 import './App.css';
+import { useState } from 'react';
+import { AuthContext, AuthContextData } from 'AuthContext';
 
 function App() {
+
+  const [authContextData, setAuthContextData] = useState<AuthContextData>({
+    authenticated: false
+  });
+
   return (
     <div className="App">
-        <Routes />
+      <AuthContext.Provider value={{authContextData, setAuthContextData}}>
+         <Routes />
+      </AuthContext.Provider>
     </div>
   );
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,15 +1,51 @@
-import { getAuthData } from "util/storage";
+import { AuthContext } from "AuthContext";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { getTokenData, isAuthenticated } from "util/auth";
+import { removeAuthData } from "util/storage";
 import "./styles.css";
 
 const NavBar = () => {
+  
+  const { authContextData, setAuthContextData } = useContext(AuthContext);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthContextData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthContextData({
+        authenticated: false,
+      });
+    }
+  }, [setAuthContextData]);
+
+  const handleSubmit = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthContextData({
+      authenticated: false,
+    });
+
+    history.push("/");
+  };
+
   return (
     <div className="NavBar-container">
       <h2>MovieFlix</h2>
-      {getAuthData().access_token && (
-        <div className="btn-container">
-          <a href="#SAIR">
-            SAIR
-          </a>
+      {authContextData.authenticated && (
+        <div className="welcome-container">
+          <span>Bem vindo(a) {authContextData.tokenData?.user_name}</span>
+          <div className="btn-container">
+            <a onClick={handleSubmit} href="#SAIR">
+              SAIR
+            </a>
+          </div>
         </div>
       )}
     </div>
